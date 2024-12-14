a7b4dfcef9e158e9476b09ac477169eb8a41b430
dhanushk240206 <dhanushk.cs24@bmsce.ac.in>
2024-12-14 19:53:13 +0530

    Create thread.java

---
+++ Program-08/thread.java
@@ -0,0 +1,36 @@
+class CollegeThread extends Thread {
+    public void run() {
+        try {
+            while (true) {
+                System.out.println("BMS College of Engineering");
+                Thread.sleep(10000); 
+            }
+        } catch (InterruptedException e) {
+            System.out.println("CollegeThread interrupted.");
+        }
+    }
+}
+
+class CSEThread extends Thread {
+    public void run() {
+        try {
+            while (true) {
+                System.out.println("CSE");
+                Thread.sleep(2000); 
+            }
+        } catch (InterruptedException e) {
+            System.out.println("CSEThread interrupted.");
+        }
+    }
+}
+
+public class ThreadDisplayProgram {
+    public static void main(String[] args) {
+        // Create and start the threads
+        CollegeThread collegeThread = new CollegeThread();
+        CSEThread cseThread = new CSEThread();
+
+        collegeThread.start();
+        cseThread.start();
+    }
+}
